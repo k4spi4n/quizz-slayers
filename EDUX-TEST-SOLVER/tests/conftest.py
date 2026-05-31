b286be075a23357c251ae7d9f4dcfe1b4d5d23c4
AkasameVN26 <thaibinhakashi2@gmail.com>
2026-05-31 11:37:00 +0700

    Refactor browser_type_launch_args fixture to include additional launch arguments for improved browser performance

--- EDUX-TEST-SOLVER/tests/conftest.py
+++ EDUX-TEST-SOLVER/tests/conftest.py
@@ -1,6 +1,11 @@
-def browser_type_launch_args():
+import pytest
+
+@pytest.fixture(scope="session")
+def browser_type_launch_args(browser_type_launch_args):
     return {
+        **browser_type_launch_args,
         "args": [
+            *browser_type_launch_args.get("args", []),
             "--disable-background-timer-throttling",
             "--disable-backgrounding-occluded-windows",
             "--disable-renderer-backgrounding",
